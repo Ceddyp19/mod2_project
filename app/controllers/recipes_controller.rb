1ec8c3f7e336492a928f36be4a87878a35616d4b
allenorton <norton.allene@gmail.com>
2020-08-19 12:23:26 -0500

    update and edit WORKING, edited show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,11 +43,67 @@ class RecipesController < ApplicationController
 
 
     def edit
+        @recipe = Recipe.find(params[:id])
     end
 
     def update
+        @recipe = Recipe.find(params[:id])
+        if params[:add_ingredient]
+            unless params[:recipe][:ingredients_arrtibutes].blank?
+                for attribute in params[:recipe][:ingredients_attributes]
+                    @recipe.ingredients.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
+                end
+            end
+            @recipe.ingredients.build
+        elsif params[:remove_ingredient]
+            #byebug
+            removed_ingredients = []
+            recipe_params[:ingredients_attributes].each { |i, att| removed_ingredients << att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
+            Ingredient.delete(removed_ingredients)
+            flash[:notice] = "Ingredients removed."
+            for attribute in params[:recipe][:ingredients_attributes]
+                @recipe.ingredients.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
+            end
+        else
+            if @recipe.update_attributes(recipe_params)
+                flash[:notice] = "Successfully updated recipe."
+                return redirect_to recipe_path(@recipe)
+            end
+        end
+        render :edit       
     end
-
+###############################Copied code below#########
+# def update
+#     @recipe = Recipe.find(params[:id])
+#     if params[:add_ingredient]
+#     	# rebuild the ingredient attributes that doesn't have an id
+#     	unless params[:recipe][:ingredients_attributes].blank?
+# 	  for attribute in params[:recipe][:ingredients_attributes]
+# 	    @recipe.ingredients.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
+# 	  end
+#     	end
+#       # add one more empty ingredient attribute
+#       @recipe.ingredients.build
+#     elsif params[:remove_ingredient]
+#       # collect all marked for delete ingredient ids
+#       removed_ingredients = params[:recipe][:ingredients_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
+#       # physically delete the ingredients from database
+#       Ingredient.delete(removed_ingredients)
+#       flash[:notice] = "Ingredients removed."
+#       for attribute in params[:recipe][:ingredients_attributes]
+#       	# rebuild ingredients attributes that doesn't have an id and its _destroy attribute is not 1
+# #         @recipe.ingredients.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
+#       end
+#     else
+#       # save goes like usual
+#       if @recipe.update_attributes(params[:recipe])
+#         flash[:notice] = "Successfully updated recipe."
+#         redirect_to @recipe and return
+#       end
+#     end
+#     render :action => 'edit'
+#   end  
+######################################
     def show
         #@recipe = Recipe.find(params[:id])
     end 
@@ -64,6 +120,7 @@ class RecipesController < ApplicationController
         params.require(:recipe).permit(:name, :calories, :description, :prep_time, :style_id, :add_ingredient, :remove_ingredient, ingredients_attributes: [
             :name,
             :category,
+            :id,
             :_destroy
           ])
      end
